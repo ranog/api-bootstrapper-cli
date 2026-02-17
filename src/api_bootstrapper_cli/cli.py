46652cb34a935680fa776be5271c4d164fecfbb5
ranog <jprnogueira@yahoo.com.br>
2026-02-17 03:17:03 -0300

    feat: add add-alembic command stub

--- src/api_bootstrapper_cli/cli.py
+++ src/api_bootstrapper_cli/cli.py
@@ -1,12 +1,16 @@
 import typer
 
+from api_bootstrapper_cli.commands.add_alembic import add_alembic
+from api_bootstrapper_cli.commands.bootstrap_env import bootstrap_env
 
-app = typer.Typer(no_args_is_help=True)
 
+app = typer.Typer(
+    no_args_is_help=True,
+    help="CLI to bootstrap and manage Python API projects.",
+)
 
-@app.command()
-def bootstrap_env():
-    typer.echo("bootstrap-env: TODO")
+app.command("bootstrap-env")(bootstrap_env)
+app.command("add-alembic")(add_alembic)
 
 
 def main():
---
+++ src/api_bootstrapper_cli/commands/add_alembic.py
@@ -0,0 +1,5 @@
+import typer
+
+
+def add_alembic() -> None:
+    typer.echo("add-alembic: TODO")
